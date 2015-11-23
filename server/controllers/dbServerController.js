6c64ddf71010c1340e8482ac7a47696dbb4be0ae
Guilherme Stiebler <guilhermemst@gmail.com>
2015-11-22 22:34:50 -0200

    concatenating buffers on a single buffer

--- server/controllers/dbServerController.js
+++ server/controllers/dbServerController.js
@@ -19,15 +19,27 @@ exports.main = function(req, res, next) {
 
 exports.uploadFile = function(req, res, next) {
     var busboy = new Busboy({ headers: req.headers });
+    var allData = [];
+    var dataLen = 0; 
     
     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
         console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
       
         file.on('data', function(data) {
+            allData.push(data);
+            dataLen += data.length;
+            
             console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+            console.log(data.toString('utf-8'));
         });
       
         file.on('end', function() {
+            var buf = new Buffer(dataLen); 
+            for (var i=0, pos = 0; i < allData.length; i++) { 
+                allData[i].copy(buf, pos); 
+                pos += allData[i].length; 
+            } 
+            console.log(buf.length); 
             console.log('File [' + fieldname + '] Finished');
         });
     });
