a2d42511028fa6baf76b8584b5a69c6947040154
Guilherme Stiebler <guilhermemst@gmail.com>
2015-11-27 14:17:35 -0200

    improving layout

--- client/angular-app/controllers/CreateItemCtrl.js
+++ client/angular-app/controllers/CreateItemCtrl.js
@@ -5,21 +5,8 @@ angular.module('CreateItemCtrl', []).controller('CreateItemController',
     var modelName = $routeParams.model;
     server.getModelFields(modelName, fieldsArrived, onError);
     
-    var htmlInputObjs = [];
     function fieldsArrived(fields) {
-        console.log(fields);
-        
-        var baseDiv = document.getElementById('baseDiv');
-        
-        for( var i = 0; i < fields.length; i++ ) {
-            var e = document.createElement('div');
-            e.innerHTML = fields[i].label + ": <input type=\"text\" \><br>";
-            baseDiv.appendChild(e);
-            htmlInputObjs.push({
-                field: fields[i],
-                htmlElement: e
-            });
-        }
+        $scope.fields = fields;
     }
     
     function onError(err) {
