f43f30bfd19a511a6da0d0ca4363e9b64c3f4b93
Guilherme Stiebler <guilhermemst@gmail.com>
2015-12-04 23:16:42 -0200

    hardcoded datatables pt-br translation

--- client/angular-app/controllers/ModelViewCtrl.js
+++ client/angular-app/controllers/ModelViewCtrl.js
@@ -7,6 +7,30 @@ angular.module('ModelViewCtrl', ['ngFileUpload']).controller('ModelViewControlle
     
     var datatableInitialized = false;
     var dataTableElement = $('#mainTable');
+    
+    var datatables_pt_br_translation = {
+        "sEmptyTable": "Nenhum registro encontrado",
+        "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+        "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sInfoThousands": ".",
+        "sLengthMenu": "_MENU_ resultados por página",
+        "sLoadingRecords": "Carregando...",
+        "sProcessing": "Processando...",
+        "sZeroRecords": "Nenhum registro encontrado",
+        "sSearch": "Pesquisar",
+        "oPaginate": {
+            "sNext": "Próximo",
+            "sPrevious": "Anterior",
+            "sFirst": "Primeiro",
+            "sLast": "Último"
+        },
+        "oAria": {
+            "sSortAscending": ": Ordenar colunas de forma ascendente",
+            "sSortDescending": ": Ordenar colunas de forma descendente"
+        }
+    };
 
     function showModel(model) {
     
@@ -27,9 +51,7 @@ angular.module('ModelViewCtrl', ['ngFileUpload']).controller('ModelViewControlle
             
             dataTableElement.DataTable( {
                 columns: columns,
-                language: {
-                    url: "http://cdn.datatables.net/plug-ins/1.10.10/i18n/Portuguese-Brasil.json"
-                }
+                language: datatables_pt_br_translation
             } );
             datatableInitialized = true;
         }
