1153a39e706360a665550d93b60c6fd26a52601d
Guilherme Stiebler <guilhermemst@gmail.com>
2015-12-06 20:56:10 -0200

    separating tests prototype of file upload test

--- test/selenium.js
+++ test/selenium.js
@@ -9,10 +9,6 @@ var port = 3333;
 var webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
-
-var driver = new webdriver.Builder()
-    .forBrowser('firefox')
-    .build();
     
 var tableLoadTime = 800;
     
@@ -29,14 +25,14 @@ function setUpServer() {
     return server;
 }
 
-function makeLogin(test) {
+function makeLogin(test, driver) {
     driver.findElement(By.name('username')).sendKeys('gstiebler');
     driver.findElement(By.name('password')).sendKeys('guilherme');
     driver.findElement(By.id('buttonOk')).click();
     test.equal('Insider Oil', await( driver.getTitle() ));
 }
 
-function addWell(test) {
+function addWell(test, driver) {
     driver.findElement(elementByText('Adicionar')).click();
     driver.sleep(200);
     test.ok( await( driver.isElementPresent(elementByText('Poço:'))) );
@@ -52,24 +48,24 @@ function addWell(test) {
     driver.findElement(elementByText('Salvar')).click();
     driver.sleep(tableLoadTime);
     test.ok( await( driver.isElementPresent(elementByText('Mostrando de 1 até 4 de 4 registros'))) );
-    test.equal( 'Novo poço Selenium', getTableValue(3, 0) );
-    test.equal( 'Operador Petrobrás', getTableValue(3, 1) );
-    test.equal( 'AC', getTableValue(3, 2) );
-    test.equal( 'Bacia do Selenium', getTableValue(3, 3) );
+    test.equal( 'Novo poço Selenium', getTableValue(3, 0, driver) );
+    test.equal( 'Operador Petrobrás', getTableValue(3, 1, driver) );
+    test.equal( 'AC', getTableValue(3, 2, driver) );
+    test.equal( 'Bacia do Selenium', getTableValue(3, 3, driver) );
 }
 
-function showWells(test) {
+function showWells(test, driver) {
     driver.findElement(elementByText('Poços')).click();
     driver.sleep(500);
-    test.equal( '1A 0001 BA', getTableValue(0, 0) );
-    test.equal( 'Petrobrás', getTableValue(0, 1) );
-    test.equal( 'Recôncavo', getTableValue(0, 3) );
-    test.equal( '1AJ 0001 BA', getTableValue(2, 0) );
-    test.equal( 'Recôncavo E&P', getTableValue(2, 1) );
-    test.equal( 'Recôncavo', getTableValue(2, 3) );
+    test.equal( '1A 0001 BA', getTableValue(0, 0, driver) );
+    test.equal( 'Petrobrás', getTableValue(0, 1, driver) );
+    test.equal( 'Recôncavo', getTableValue(0, 3, driver) );
+    test.equal( '1AJ 0001 BA', getTableValue(2, 0, driver) );
+    test.equal( 'Recôncavo E&P', getTableValue(2, 1, driver) );
+    test.equal( 'Recôncavo', getTableValue(2, 3, driver) );
 }
 
-function checkMainPage(test) {
+function checkMainPage(test, driver) {
     test.ok( await( driver.isElementPresent(By.id('navbar')) ) );
     test.ok( !await( driver.isElementPresent(By.id('foca')) ) );
     test.ok( !await( driver.isElementPresent(elementByText('Focas'))) );
@@ -78,21 +74,21 @@ function checkMainPage(test) {
     test.ok( await( driver.isElementPresent(elementByText('Logout'))) );
 }
 
-function editWell(test) {
-    var editBtn = getTableCell(2, 4).findElement(By.xpath("a"));
+function editWell(test, driver) {
+    var editBtn = getTableCell(2, 4, driver).findElement(By.xpath("a"));
     editBtn.click();
     driver.sleep(200);
     driver.findElement(By.id('html_id_Operador')).clear();
     driver.findElement(By.id('html_id_Operador')).sendKeys('Operador Elvis Foca');
     driver.findElement(elementByText('Salvar')).click();
     driver.sleep(tableLoadTime);
-    test.equal( '1AJ 0001 BA', getTableValue(2, 0) );
-    test.equal( 'Operador Elvis Foca', getTableValue(2, 1) );
-    test.equal( 'Recôncavo', getTableValue(2, 3) );
+    test.equal( '1AJ 0001 BA', getTableValue(2, 0, driver) );
+    test.equal( 'Operador Elvis Foca', getTableValue(2, 1, driver) );
+    test.equal( 'Recôncavo', getTableValue(2, 3, driver) );
 }
 
-function deleteWell(test) {
-    var deleteBtn = getTableCell(2, 5).findElement(By.xpath("button"));
+function deleteWell(test, driver) {
+    var deleteBtn = getTableCell(2, 5, driver).findElement(By.xpath("button"));
     deleteBtn.click();
     driver.switchTo().alert().accept();
     driver.sleep(tableLoadTime);
@@ -100,7 +96,11 @@ function deleteWell(test) {
     test.ok( !await( driver.isElementPresent(elementByText('Operador Elvis Foca'))) );
 }
 
-function logout(test) {
+function uploadExcelFile(test, driver) {
+    
+}
+
+function logout(test, driver) {
     driver.findElement(elementByText('Logout')).click();
     test.equal('Login', await( driver.getTitle() ));
 }
@@ -110,16 +110,40 @@ var group = {
 first: function(test) {
     var server = setUpServer();
     
+    var driver = new webdriver.Builder()
+        .forBrowser('firefox')
+        .build();
+    
     driver.get('http://localhost:' + port);
     test.equal('Login', await( driver.getTitle() ));
     
-    makeLogin(test);
-    checkMainPage(test);
-    showWells(test);
-    addWell(test);
-    editWell(test);
-    deleteWell(test);
-    logout(test);
+    makeLogin(test, driver);
+    checkMainPage(test, driver);
+    showWells(test, driver);
+    addWell(test, driver);
+    editWell(test, driver);
+    deleteWell(test, driver);
+    logout(test, driver);
+    
+    server.close();
+    driver.quit();
+    test.done();
+},
+
+uploadExcelFiles: function(test) {
+    var server = setUpServer();
+    
+    var driver = new webdriver.Builder()
+        .forBrowser('firefox')
+        .build();
+    
+    driver.get('http://localhost:' + port);
+    test.equal('Login', await( driver.getTitle() ));
+        
+    makeLogin(test, driver);
+    
+    showWells(test, driver);
+    uploadExcelFile(test, driver);
     
     server.close();
     driver.quit();
@@ -129,19 +153,19 @@ first: function(test) {
 };
 
 
-function getTableRows() {
+function getTableRows(driver) {
     return await( driver.findElements(By.xpath("id('mainTable')/tbody/tr")) );
 }
 
 
-function getTableCell(row, columnn) {
-    var tds = await( getTableRows()[row].findElements(By.xpath("td")) );
+function getTableCell(row, columnn, driver) {
+    var tds = await( getTableRows(driver)[row].findElements(By.xpath("td")) );
     return tds[columnn]
 }
 
 
-function getTableValue(row, columnn) {
-    var text = await( getTableCell(row, columnn).getText() );
+function getTableValue(row, columnn, driver) {
+    var text = await( getTableCell(row, columnn, driver).getText() );
     return text;
 }
 
