caac5c13fed2bc84c5320c68819e4b84d101e2ea
Guilherme Stiebler <guilhermemst@gmail.com>
2015-12-06 17:16:57 -0200

    simplifying tests

--- test/selenium.js
+++ test/selenium.js
@@ -36,14 +36,14 @@ first: function(test) {
     test.equal('Login', await( driver.getTitle() ));
     
     // login
-    driver.findElement(webdriver.By.name('username')).sendKeys('gstiebler');
-    driver.findElement(webdriver.By.name('password')).sendKeys('guilherme');
-    driver.findElement(webdriver.By.id('buttonOk')).click();
+    driver.findElement(By.name('username')).sendKeys('gstiebler');
+    driver.findElement(By.name('password')).sendKeys('guilherme');
+    driver.findElement(By.id('buttonOk')).click();
     test.equal('Insider Oil', await( driver.getTitle() ));
     
     //basic check on main page
-    test.ok( isElementPresentSync(webdriver.By.id('navbar')) );
-    test.ok( !isElementPresentSync(webdriver.By.id('foca')) );
+    test.ok( isElementPresentSync(By.id('navbar')) );
+    test.ok( !isElementPresentSync(By.id('foca')) );
     test.ok( !isElementPresentSync( elementByText('Focas') ) );
     test.ok( isElementPresentSync( elementByText('Poços') ) );
     test.ok( isElementPresentSync( elementByText('Sondas') ) );
@@ -62,12 +62,12 @@ first: function(test) {
     test.ok( isElementPresentSync( elementByText('Operador:') ) );
     test.ok( isElementPresentSync( elementByText('Longitude') ) );
     test.ok( isElementPresentSync( elementByText('Salvar') ) );
-    driver.findElement(webdriver.By.id('html_id_Poço')).sendKeys('Novo poço Selenium');
-    driver.findElement(webdriver.By.id('html_id_Operador')).sendKeys('Operador Petrobrás');
-    driver.findElement(webdriver.By.id('html_id_Estado')).sendKeys('AC');
-    driver.findElement(webdriver.By.id('html_id_Bacia')).sendKeys('Bacia do Selenium');
-    driver.findElement(webdriver.By.id('html_id_Latitude')).sendKeys('1234');
-    driver.findElement(webdriver.By.id('html_id_Longitude')).sendKeys('3334321');
+    driver.findElement(By.id('html_id_Poço')).sendKeys('Novo poço Selenium');
+    driver.findElement(By.id('html_id_Operador')).sendKeys('Operador Petrobrás');
+    driver.findElement(By.id('html_id_Estado')).sendKeys('AC');
+    driver.findElement(By.id('html_id_Bacia')).sendKeys('Bacia do Selenium');
+    driver.findElement(By.id('html_id_Latitude')).sendKeys('1234');
+    driver.findElement(By.id('html_id_Longitude')).sendKeys('3334321');
     driver.findElement(elementByText('Salvar')).click();
     sleep.sync(null, 800);
     test.ok( isElementPresentSync( elementByText('Mostrando de 1 até 4 de 4 registros') ) );
@@ -92,7 +92,7 @@ function sleep(ms, callback) {
 
 function elementByText(text) {
     var str = "//*[contains(text(), '" + text + "')]";
-    return webdriver.By.xpath(str)
+    return By.xpath(str)
 }
 
 function isElementPresentSync(by) {
