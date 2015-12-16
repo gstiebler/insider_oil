111f267158fbacb73926cc3004e84cb53e8ebd1f
Guilherme Stiebler <guilhermemst@gmail.com>
2015-12-16 16:09:02 -0200

    refactoring on controller test to make it more sync

--- test/controllers.js
+++ test/controllers.js
@@ -1,7 +1,34 @@
 "use strict";
 var fiberTests = require('./lib/fiberTests');
 var dbServerController = require('../controllers/dbServerController');
+//var Sync = require('sync');
 
+function deStringify(json) {
+    var str = JSON.stringify(json);
+    return JSON.parse(str);
+}
+
+function getJsonResponse(func, req, callback) {
+    const res = { 
+        json: jsonRes,
+        status: status
+    };
+    func(req, res);
+    
+    function jsonRes(response) {
+        callback(null, deStringify(response) );
+    }
+    
+    function status(code) {
+        console.log('code:', code);
+        const result = { 
+            json: function(response) { 
+                callback(null, code, deStringify(response) );
+            } 
+        };
+        return result;
+    }
+}
 
 var group = {
 
@@ -9,24 +36,19 @@ listWells: function(test) {
     const req = {
         query: { table: 'Well' }
     };
-    const res = { json: jsonRes };
-    dbServerController.main(req, res);
     
-    function jsonRes(response) {
-        var str = JSON.stringify(response);
-        var objBack = JSON.parse(str);
-        // records
-        test.equal(3, objBack.records.length);
-        test.equal('1A 0001 BA', objBack.records[0].name);
-        test.equal('Petrobrás', objBack.records[0].operator_name);
-        // view params
-        test.equal( 'Poços', response.viewParams.tableLabel );
-        test.equal( 'name', response.viewParams.labelField );
-        test.equal( 'Poço', response.viewParams.fields.name.label );
-        test.equal( 'Operador', response.viewParams.fields.operator_name.label );
-        test.equal( 'Latitude', response.viewParams.fields.lat.label );
-        test.done();
-    }
+    const response = getJsonResponse.sync(null, dbServerController.main, req);
+    // records
+    test.equal(3, response.records.length);
+    test.equal('1A 0001 BA', response.records[0].name);
+    test.equal('Petrobrás', response.records[0].operator_name);
+    // view params
+    test.equal( 'Poços', response.viewParams.tableLabel );
+    test.equal( 'name', response.viewParams.labelField );
+    test.equal( 'Poço', response.viewParams.fields.name.label );
+    test.equal( 'Operador', response.viewParams.fields.operator_name.label );
+    test.equal( 'Latitude', response.viewParams.fields.lat.label );
+    test.done();
 },
 
 
@@ -34,22 +56,20 @@ modelFields: function(test) {
     const req = {
         query: { model: 'Well' }
     };
-    const res = { json: jsonRes };
-    dbServerController.modelFields(req, res)
     
-    function jsonRes(response) {
-        test.equal( 'name', response.fields[0].name );
-        test.equal( 'Poço', response.fields[0].label );
-        test.equal( 'VARCHAR(255)', response.fields[0].type );
-        test.equal( 'lat', response.fields[3].name );
-        test.equal( 'Latitude', response.fields[3].label );
-        test.equal( 'DECIMAL', response.fields[3].type );
-        test.equal( 'operator_id', response.fields[5].name );
-        test.equal( 'Operador', response.fields[5].label );
-        test.equal( 'ref', response.fields[5].type );
-        test.equal( 'Company', response.fields[5].model );
-        test.done();
-    }
+    const response = getJsonResponse.sync(null, dbServerController.modelFields, req);
+    
+    test.equal( 'name', response.fields[0].name );
+    test.equal( 'Poço', response.fields[0].label );
+    test.equal( 'VARCHAR(255)', response.fields[0].type );
+    test.equal( 'lat', response.fields[3].name );
+    test.equal( 'Latitude', response.fields[3].label );
+    test.equal( 'DECIMAL', response.fields[3].type );
+    test.equal( 'operator_id', response.fields[5].name );
+    test.equal( 'Operador', response.fields[5].label );
+    test.equal( 'ref', response.fields[5].type );
+    test.equal( 'Company', response.fields[5].model );
+    test.done();
 },
 
 
@@ -57,21 +77,19 @@ getComboValues: function(test) {
     const req = {
         query: { model: 'Company' }
     };
-    const res = { json: jsonRes };
-    dbServerController.getComboValues(req, res)
+
+    const response = getJsonResponse.sync(null, dbServerController.getComboValues, req);
     
-    function jsonRes(response) {
-        test.equal( 1, response[0].id );
-        test.equal( 2, response[1].id );
-        test.equal( 6, response[5].id );
-        test.equal( 7, response[6].id );
-        
-        test.equal( 'Petrobrás', response[0].label );
-        test.equal( 'Eni Oil', response[1].label );
-        test.equal( 'Schahin', response[5].label );
-        test.equal( 'Paragon', response[6].label );
-        test.done();
-    }
+    test.equal( 1, response[0].id );
+    test.equal( 2, response[1].id );
+    test.equal( 6, response[5].id );
+    test.equal( 7, response[6].id );
+    
+    test.equal( 'Petrobrás', response[0].label );
+    test.equal( 'Eni Oil', response[1].label );
+    test.equal( 'Schahin', response[5].label );
+    test.equal( 'Paragon', response[6].label );
+    test.done();
 },
 
 
@@ -89,23 +107,17 @@ createWell: function(test) {
             }
         }
     };
-    const res = { json: function(){} };
-    dbServerController.createItem(req, res);
     
-    res.json = jsonRes;
+    getJsonResponse.sync(null, dbServerController.createItem, req);
+    
     const req2 = {
         query: { table: 'Well' }
     };
-    dbServerController.main(req2, res);
-    
-    function jsonRes(response) {
-        var str = JSON.stringify(response);
-        var objBack = JSON.parse(str);
-        test.equal(4, response.records.length);
-        test.equal('Novo poço', objBack.records[3].name);
-        test.equal('Statoil', objBack.records[3].operator_name);
-        test.done();
-    }
+    const response = getJsonResponse.sync(null, dbServerController.main, req2);
+    test.equal(4, response.records.length);
+    test.equal('Novo poço', response.records[3].name);
+    test.equal('Statoil', response.records[3].operator_name);
+    test.done();
 },
 
 
@@ -124,26 +136,21 @@ editWell: function(test) {
             }
         }
     };
-    const res = { json: function(response){ 
-        test.equal('OK', response.msg);
-        
-        const res2 = { json: jsonRes };
-        const req2 = {
-            query: { 
-                model: 'Well',
-                id: 2
-            }
-        };
-        dbServerController.recordValues(req2, res2);
-    }};
-    dbServerController.saveItem(req, res);
     
-    function jsonRes(response) {
-        test.equal('Novo poço', response.values.name);
-        test.equal(4, response.values.operator_id);
-        test.equal(444, response.values.lng);
-        test.done();
-    }
+    const response = getJsonResponse.sync(null, dbServerController.saveItem, req);
+    test.equal('OK', response.msg);
+    
+    const req2 = {
+        query: { 
+            model: 'Well',
+            id: 2
+        }
+    };
+    const response2 = getJsonResponse.sync(null, dbServerController.recordValues, req2);
+    test.equal('Novo poço', response2.values.name);
+    test.equal(4, response2.values.operator_id);
+    test.equal(444, response2.values.lng);
+    test.done();
 },
 
 
@@ -154,19 +161,15 @@ deleteWell: function(test) {
             id: 2
         }
     };
-    const res = { json: function(response){ test.equal('OK', response.msg) } };
-    dbServerController.deleteItem(req, res);
+    const response = getJsonResponse.sync(null, dbServerController.deleteItem, req);
+    test.equal('OK', response.msg);
     
     const req2 = {
         query: { table: 'Well' }
     };
-    const res2 = { json: jsonRes };
-    dbServerController.main(req2, res2);
-    
-    function jsonRes(response) {
-        test.equal(2, response.records.length);
-        test.done();
-    }
+    const response2 = getJsonResponse.sync(null, dbServerController.main, req2);
+    test.equal(2, response2.records.length);
+    test.done();
 }
 
 };
