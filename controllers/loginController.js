75abd71bd7d781fa09f4f6dc6c994166ddb77852
Guilherme Stiebler <guilhermemst@gmail.com>
2015-12-18 12:38:28 -0200

    showing error messages on login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,7 +2,7 @@ var session = require('../lib/session');
 
 
 exports.loginPage = function(req, res, next) {
-    res.render('login', { title: 'Login' });
+    res.render('login');
 };
 
 
@@ -10,11 +10,11 @@ exports.makeLogin = function(req, res, next) {
     session.login( req.body.username, req.body.password, loginOk, loginError );
     
     function loginOk(token) {
-        return res.redirect('/app/templates/index.html?token=' + token);
+        res.redirect('/app/templates/index.html?token=' + token);
     }
     
-    function loginError() {
-        return res.redirect('/login');
+    function loginError(errorMsg) {
+        res.render('login', { errorMsg: errorMsg });
     }
 };
 
