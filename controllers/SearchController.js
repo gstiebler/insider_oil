695235f63ac6ddf15e8138f1b0825463144314c9
Guilherme Stiebler <guilhermemst@gmail.com>
2016-01-08 10:13:19 -0200

    changing search for only one query

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -2,6 +2,29 @@
 var db  = require('../db/models');
 var await = require('../lib/await');
 
+
+function search(searchValue, dataSources, numMaxResults) {
+	var results = [];
+    const searchStr = '%' + searchValue + '%';
+    var queryStrings = [];
+    for( var i = 0; i < dataSources.length; i++) {
+    	const dataSource = dataSources[i];
+    	for( var j = 0; j < dataSource.fields.length; j++ ) {
+    		const model = db[dataSource.model];
+    		const fieldName = dataSource.fields[j];
+    		var currQuery = 'select ' + fieldName + ' as name, "' + dataSource.model + '" as model, id from ' + model.getTableName();
+    		currQuery += ' where ' + fieldName + ' like "' + searchStr + '"';
+    		queryStrings.push(currQuery);
+    		queryStrings.push('\n union \n')
+    	}
+    }
+    
+    queryStrings[queryStrings.length - 1] = '\n limit ' + numMaxResults;
+    const queryStr = queryStrings.join('');
+    return db.sequelize.query(queryStr);
+}
+
+
 exports.main = function(req, res) {
 	const MAX_NUM_RESULTS = 5;
     const searchValue = req.query.searchValue;
@@ -51,31 +74,18 @@ exports.main = function(req, res) {
         	fields: ['name']
         }
     ];
-    var results = [];
-    for( var i = 0; i < dataSources.length; i++) {
-    	const dataSource = dataSources[i];
-    	for( var j = 0; j < dataSource.fields.length; j++ ) {
-    		const model = db[dataSource.model];
-    		const fieldName = dataSource.fields[j];
-    		const options = { 
-    			where: { },
-    			limit: MAX_NUM_RESULTS - results.length
-    		};
-            const searchStr = '%' + searchValue + '%';
-    		options.where[fieldName] = { $like: searchStr } ;
-    		const currentResults = await( model.findAll(options) );
-    		for( var n = 0; n < currentResults.length; n++ ) {
-    			results.push({
-    				model: dataSource.model,
-    				name: currentResults[n][fieldName],
-    				id: currentResults[n].id
-    			});
-    		}
-    		if(results.length >= MAX_NUM_RESULTS) {
-    			res.json(results);
-    			return;
-    		}
-    	}
+    
+    search(searchValue, dataSources, MAX_NUM_RESULTS).then(onResults);
+    function onResults(queryResults) {
+    	var results = [];
+		for( var n = 0; n < queryResults[0].length; n++ ) {
+			const result = queryResults[0][n];
+			results.push({
+				model: result.model,
+				name: result.name,
+				id: result.id
+			});
+		}
+        res.json(results);
     }
-    res.json(results);
 };
