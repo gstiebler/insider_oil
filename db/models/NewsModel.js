adcd56ea13e8d743f5f3249c02b9020967f6db60
Guilherme Stiebler <guilhermemst@gmail.com>
2016-01-17 13:39:50 -0200

    commentaries

--- db/models/NewsModel.js
+++ db/models/NewsModel.js
@@ -5,6 +5,7 @@ function afterCreate(db) {
 	db.News.hook('afterCreate', function(news, options) {
 		const referencedObjects = newsLib.getModelReferences(news.content);
 		const promiseModelIdArray = [];
+		// gets the ids from the models in ModelsList
 		for(var i = 0; i < referencedObjects.length; i++) {
 			const referencedObj = referencedObjects[i];
 			const findOptions = { where: { name: referencedObj.model } };
@@ -14,6 +15,7 @@ function afterCreate(db) {
 		return Promise.all(promiseModelIdArray).then( function(modelIds) {
 			try {
 				const promiseArray = [];
+				// insert the records in NewsModels
 				for(var i = 0; i < referencedObjects.length; i++) {
 					const referencedObj = referencedObjects[i];
 					const modelId = modelIds[i].id;
