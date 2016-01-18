8492c8de380370de26176fcdd2c3750a394ed0fb
Guilherme Stiebler <guilhermemst@gmail.com>
2016-01-17 23:10:04 -0200

    not dropping all tables in initialize db. It´s done in the migrations now

--- db/seeders/initialize_db.js
+++ db/seeders/initialize_db.js
@@ -3,14 +3,10 @@ var db = require( '../models' );
 var Sync = require('sync');
 var createFixtures = require('../../test/fixtures/initial_data');
 
-db.sequelize.getQueryInterface().dropAllTables().then( function() {
-    db.sequelize.sync().then(function() {
-        Sync(function() {
-            try {
-                createFixtures();
-            } catch(e) {
-                console.error(e.stack);
-            }
-        });
-    });
+Sync(function() {
+    try {
+        createFixtures();
+    } catch(e) {
+        console.error(e.stack);
+    }
 });
