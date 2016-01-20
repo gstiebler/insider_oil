64afff1fb97b16fe5523684696f30e6788919c9a
Guilherme Stiebler <guilhermemst@gmail.com>
2016-01-20 16:58:21 -0200

    formatting news in objects

--- client/angular-app/controllers/ViewRecordCtrl.js
+++ client/angular-app/controllers/ViewRecordCtrl.js
@@ -12,8 +12,17 @@ angular.module('ViewRecordCtrl', ['flash', 'ui.bootstrap', 'ngSanitize']).contro
     }
     server.viewRecord( source, id, showValues, showError.show );
         
+    function parseDateTime(dateTimeStr) {
+    	const date = new Date(dateTimeStr);
+    	return date.toLocaleString();
+    }
+    
     // show record news
     function showNews(news) {
+    	for(var i = 0; i < news.length; i++) {
+    		news[i].formattedCreatedAt = parseDateTime(news[i].created_at);
+    	}
+    	
     	$scope.news = news;
     }
     server.newsFromObject(source, id, showNews, showError.show);
