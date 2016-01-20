39374136cb0f8f4468cb7ccaaf8d11b8cfbf67b1
Guilherme Stiebler <guilhermemst@gmail.com>
2016-01-20 16:58:39 -0200

    reseting db script in development

--- db/seeders/initialize_db.js
+++ db/seeders/initialize_db.js
@@ -1,10 +1,20 @@
 var db = require( '../models' );
-
+var await = require('../../lib/await');
+var Umzug = require('umzug');
 var Sync = require('sync');
 var createFixtures = require('../../test/fixtures/initial_data');
 
+var umzug = new Umzug({
+	migrations: {
+		path: __dirname + '/../../db/migrations',
+		params: [db.sequelize.getQueryInterface(), db.Sequelize]
+	}
+});
+
 Sync(function() {
     try {
+        await( umzug.down() );
+        await( umzug.up() );
         createFixtures();
     } catch(e) {
         console.error(e.stack);
