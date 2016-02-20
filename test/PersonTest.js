f244371f2694ad8cfb715954fea245dfc213d282
Guilherme Stiebler <guilhermemst@gmail.com>
2016-02-20 15:43:22 -0200

    person test

---
+++ test/PersonTest.js
@@ -0,0 +1,37 @@
+"use strict";
+const fiberTests = require('./lib/fiberTests');
+const utils = require('./lib/utils');
+var db = require('../db/models');
+var dbServerController = require('../controllers/dbServerController');
+var await = require('../lib/await');
+
+
+var group = {
+
+getPerson: test => {
+    const req = {
+        query: { 
+            model: 'Person',
+            id: 2
+        }
+    };
+    const response = utils.getJsonResponse.sync(null, dbServerController.recordValues, req);
+    console.log(response);
+    test.equal('Felipe', response.values.name);
+    test.equal(2, response.values.company_id);
+    
+    test.equal('Nome', response.fields[0].label);
+    test.equal('name', response.fields[0].name);
+    test.equal('E-mail', response.fields[2].label);
+    test.equal('email', response.fields[2].name);
+    
+    const telephones = ['+55 21 234-5678', '98989-9498'];
+    test.equal(telephones, response.values.telephones);
+    
+    test.done();
+}
+
+};
+
+
+fiberTests.convertTests( exports, group );
