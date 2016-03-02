2c8589d84a206f3368dc30c991eecc5da8d883e6
Guilherme Stiebler <guilhermemst@gmail.com>
2016-03-02 17:27:07 -0300

    Get projects function in PersonProjects model

--- db/models/PersonProjectsModel.js
+++ db/models/PersonProjectsModel.js
@@ -1,3 +1,37 @@
+"use strict";
+
+function getProjects(sequelize, personId) {
+    var modelRefsQueryStr = 'select m.id as model_id, p.model_ref_id as id, m.name as model ';
+    modelRefsQueryStr += 'from person_projects p, models_list m ';
+    modelRefsQueryStr += 'where p.model_id = m.id ';
+    modelRefsQueryStr += 'and person_id = ' + personId;
+    const simpleQueryType = { type: sequelize.QueryTypes.SELECT};
+    return sequelize.query(modelRefsQueryStr, simpleQueryType).then( (modelRefs) => {
+        const queryStrings = []
+        for(var i = 0; i < modelRefs.length; i++) {
+            const modelRefRecord = modelRefs[i];
+            const modelName = modelRefRecord.model;
+            const model = sequelize.models[modelName];
+            const tableName = model.tableName;
+            // TODO get the correct label field
+            const labelField = 'name';
+            var modelValsQueryStr = 'select ' + modelRefRecord.model_id + ' as model_id, ';
+            modelValsQueryStr += '"' + modelRefRecord.model + '" as model, ';
+            modelValsQueryStr += 'id, ';
+            modelValsQueryStr += labelField +' ';
+            modelValsQueryStr += 'from ' + tableName;
+            modelValsQueryStr += ' where id = ' + modelRefRecord.id + '\n';
+            queryStrings.push(modelValsQueryStr);
+            queryStrings.push('\n union \n');
+        }
+        if(modelRefs.length) {
+            queryStrings.pop();
+            const queryStr = queryStrings.join('');
+            return sequelize.query(queryStr, simpleQueryType);
+        }
+    });
+}
+
 module.exports = function(sequelize, DataTypes) {
 	var PersonProjects = sequelize.define('PersonProjects', {
 		model_ref_id : {
@@ -15,12 +49,13 @@ module.exports = function(sequelize, DataTypes) {
 	}, 
 	{
 		underscored : true,
-		tableName : 'person_projects',
-		classMethods : {
-			associate : function(models) {
+		tableName: 'person_projects',
+		classMethods: {
+			associate: function(models) {
 				PersonProjects.belongsTo(models.Person, { as : 'person' });
 				PersonProjects.belongsTo(models.ModelsList, { as : 'model' });
-			}
+			},
+            getProjects: getProjects
 		}
 	});
 
