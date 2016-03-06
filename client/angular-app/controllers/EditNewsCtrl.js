2906ca901ce658d59a823d4e99e51d6616f6bde7
Guilherme Stiebler <guilhermemst@gmail.com>
2016-03-06 15:09:12 -0300

    fix new line before link in editing news

--- client/angular-app/controllers/EditNewsCtrl.js
+++ client/angular-app/controllers/EditNewsCtrl.js
@@ -26,12 +26,20 @@ angular.module('InsiderOilApp').controller('EditNewsController',
 	$scope.printHtml = function() {
 		console.log($scope.content);
 	}
+    
+    
+    function insertLinkInContent(previousContent, linkStr) {
+        const symbolPos = previousContent.lastIndexOf('<');
+        const beforeSymbolStr = previousContent.slice(0, symbolPos);
+        const afterSymboStr = previousContent.slice(symbolPos, previousContent.length);
+        return beforeSymbolStr + linkStr + afterSymboStr;
+    }
 
     
     $scope.showError = showError.show;
     $scope.onProjectSelected = function(selectedItem) {
     	const linkStr = '<a href="/app/view_record?source=' + selectedItem.model + '&id=' + selectedItem.id + '">' + selectedItem.name + '</a>';
-    	$scope.content += linkStr;
+    	$scope.content = insertLinkInContent($scope.content, linkStr);
     }
                 	
 }]);
