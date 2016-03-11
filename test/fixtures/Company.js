8a8601e7afa85a22c93f2b73ab2ee06d285f7fa0
Guilherme Stiebler <guilhermemst@gmail.com>
2016-03-11 14:47:40 -0300

    Adding companies to fixture

--- test/fixtures/Company.js
+++ test/fixtures/Company.js
@@ -1,7 +1,7 @@
 module.exports = function(db) {
     return db.Company.bulkCreate([
         {
-            name: "Petrobrás",
+            name: "Petrobras",
             address: "Rua no Centro"
         },
         {
@@ -22,6 +22,123 @@ module.exports = function(db) {
         },
         {
             name: "Paragon"
+        },
+        {
+            name: 'TOG Brasil'
+        },
+        {
+            name: 'Geopark Brasil'
+        },
+        {
+            name: 'Nova Petróleo'
+        },
+        {
+            name: 'Imetame'
+        },
+        {
+            name: 'G3 Óleo e Gás'
+        },
+        {
+            name: 'Petra Energia'
+        },
+        {
+            name: 'BG Brasil'
+        },
+        {
+            name: 'Ouro Preto'
+        },
+        {
+            name: 'Chariot Brasil'
+        },
+        {
+            name: 'BP Energy'
+        },
+        {
+            name: 'Repsol Sinopec'
+        },
+        {
+            name: 'Anadarko'
+        },
+        {
+            name: 'Chevron Frade'
+        },
+        {
+            name: 'ExxonMobil Brasil'
+        },
+        {
+            name: 'Total E&P do Brasil'
+        },
+        {
+            name: 'Premier Oil Brasil'
+        },
+        {
+            name: 'Statoil Brasil'
+        },
+        {
+            name: 'Perenco Brasil'
+        },
+        {
+            name: 'Cowan Petróleo e Gás'
+        },
+        {
+            name: 'Vipetro'
+        },
+        {
+            name: 'Brasoil Manati'
+        },
+        {
+            name: 'Queiroz Galvão'
+        },
+        {
+            name: 'BHP Billiton Brasil'
+        },
+        {
+            name: 'Parnaíba Gás Natural'
+        },
+        {
+            name: 'Galp Energia Brasil'
+        },
+        {
+            name: 'Niko Brasil'
+        },
+        {
+            name: 'UTC EP'
+        },
+        {
+            name: 'SHB'
+        },
+        {
+            name: 'Oil M&S'
+        },
+        {
+            name: 'Gran Tierra'
+        },
+        {
+            name: 'Karoon'
+        },
+        {
+            name: 'Rosneft'
+        },
+        {
+            name: 'Ecopetrol Óleo e Gás'
+        },
+        {
+            name: 'BPMB Parnaíba'
+        },
+        {
+            name: 'Alvopetro'
+        },
+        {
+            name: 'Petrosynergy'
+        },
+        {
+            name: 'Tek'
+        },
+        {
+            name: 'Shell Brasil'
+        },
+        {
+            name: 'Cisco'
         }
     ]);
 }
