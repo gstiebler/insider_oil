d829ace27d5904cd6c8e8bb586f8a8aea154e4ce
Guilherme Stiebler <guilhermemst@gmail.com>
2016-03-11 18:14:33 -0300

    Fix Seismic date format in fixture

--- test/fixtures/Seismic.js
+++ test/fixtures/Seismic.js
@@ -5,24 +5,24 @@ module.exports = function(db) {
         {
             process: '48610.014844/2012-28',
             authorized_company: 'Strataimage Consultoria Ltda.',
-            dou_publi_date: '1/9/2013',
-            end_date: '3/9/2013',
+            dou_publi_date: '2013-01-09',
+            end_date: '2013-03-09',
             authorized_technologies: 'Aquisição de Dados Magnetotelúricos',
             basin_id: utils.idByName('Basin', 'Potiguar')
         },
         {
             process: '48610.000189/2013-10',
             authorized_company: 'PGS Investigação Petrolífera Ltda.',
-            dou_publi_date: '1/14/2013',
-            end_date: '1/14/2014',
+            dou_publi_date: '2013-01-14',
+            end_date: '2014-01-14',
             authorized_technologies: 'Reprocessamento de Dados Sísmicas 3D',
             basin_id: utils.idByName('Basin', 'Tucano Central')
         },
         {
             process: '48610.000912/2013-52',
             authorized_company: 'ANDL Serviços Geof ísicos Ltda.',
-            dou_publi_date: '1/30/2013',
-            end_date: '1/30/2015',
+            dou_publi_date: '2013-01-30',
+            end_date: '2015-01-30',
             authorized_technologies: 'Aquisição de Dados',
             basin_id: utils.idByName('Basin', 'Recôncavo')
         }
