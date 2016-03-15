afe4d597cd83354465bd4684ccd958c51e28765f
Guilherme Stiebler <guilhermemst@gmail.com>
2016-03-15 16:02:17 -0300

    Add 'blocks' virtual field to Ambiental License

--- db/models/AmbientalLicense.js
+++ db/models/AmbientalLicense.js
@@ -1,3 +1,39 @@
+'use strict';
+
+function updateBlocks(db, ambientalLicense) {
+    const blocks = ambientalLicense.dataValues.blocks;
+    if(blocks == null)
+        return;
+    
+    const options = { where: { ambiental_license_id: ambientalLicense.id } };
+    // remove all blocks associated with this ambiental license
+    db.AmbientalLicenseBlock.destroy(options).then(function() {
+        const newBlocksRecords = [];
+        for(var i = 0; i < blocks.length; i++) {
+            const albRecord = { 
+                ambiental_license_id: ambientalLicense.id,
+                block_id: blocks[i].block_id
+            };
+            newBlocksRecords.push(albRecord);
+        }
+        return db.AmbientalLicenseBlock.bulkCreate(newBlocksRecords);
+    });
+}
+
+
+function updateFieldsFunc(db) {
+    return function (ambientalLicense) {
+        updateBlocks(db, ambientalLicense);
+    }
+}
+
+
+function defineHooks(db) {
+	db.AmbientalLicense.hook('afterCreate', updateFieldsFunc(db));
+	db.AmbientalLicense.hook('beforeUpdate', updateFieldsFunc(db));
+}
+
+
 module.exports = function(sequelize, DataTypes) {
   var AmbientalLicense = sequelize.define('AmbientalLicense', {
         license: {
@@ -31,11 +67,25 @@ module.exports = function(sequelize, DataTypes) {
         pac: {
           type: DataTypes.STRING,
           allowNull: false
-        }
+        },     
+        blocks: {
+            type: DataTypes.VIRTUAL,
+            get: function() {
+                var queryStr = 'select b.name, b.id ';
+                queryStr += 'from ambiental_license_blocks alb, blocks b ';
+                queryStr += 'where alb.block_id = b.id';
+                const simpleQueryType = { type: sequelize.QueryTypes.SELECT};
+                const result = await( sequelize.query(queryStr, simpleQueryType) );
+                return result;
+            }
+        },
     }, 
     {
         underscored: true,
-        tableName: 'ambiental_licenses'
+        tableName: 'ambiental_licenses',
+		classMethods: {
+			defineHooks: defineHooks
+		}
     }
   );
   
