21157e1cdd3e04641dd7367bf69ce56f3ed5f589
Guilherme Stiebler <guilhermemst@gmail.com>
2016-03-15 19:23:04 -0300

    Refactoring: brake the line in 2 to store a temporary variable

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -3,7 +3,8 @@ var db = require('../../db/models');
 
 exports.idByName = function(modelName, value) {
     const model = db[modelName];
-    return await( model.findOne( { where: { name: value }} ) ).id;
+    const record = await( model.findOne( { where: { name: value }} ) );
+    return record.id;
 }
 
 function deStringify(json) {
