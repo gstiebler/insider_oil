0e04560c7a6a59f0ad4e06b1c4bd979b1ec8b9b6
Guilherme Stiebler <guilhermemst@gmail.com>
2016-03-15 20:30:57 -0300

    including underscore.js

--- client/angular-app/directives/ListOfInputsDirective.js
+++ client/angular-app/directives/ListOfInputsDirective.js
@@ -10,13 +10,6 @@ app.directive('listOfInputs', function() {
         controller: ['$scope', function($scope) { 
             if(!$scope.modelValues)
                 $scope.modelValues = [];
-                
-            $scope.range = function(max) {
-                const res = [];
-                for(var i = 0; i < max; i++)
-                    res.push(i);
-                return res;
-            }
         }],
         templateUrl: 'app/directives/templates/list_of_inputs.html'
     };
