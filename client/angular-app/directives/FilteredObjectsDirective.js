f53d22452f01b4a2a4453fa8be18946b203a3b59
Guilherme Stiebler <guilhermemst@gmail.com>
2016-03-16 18:08:18 -0300

    Refactoring on directive

--- client/angular-app/directives/FilteredObjectsDirective.js
+++ client/angular-app/directives/FilteredObjectsDirective.js
@@ -1,7 +1,55 @@
 'use strict';
-var app = angular.module('FilteredObjectsDirective', []);
 
-app.directive('filteredObjects', ['server', function(server) {
+function controllerFunc($scope, server) {
+    const referencedObject = $scope.referencedObject;
+    const filterField = referencedObject.filterField;
+    const showFields = referencedObject.showFields;
+    const filter = {};
+    filter[filterField] = $scope.id;
+    $scope.dataSource = referencedObject.dataSource;
+    const options = { 
+        filters: filter,
+        fieldNames: showFields
+    };
+    server.getTable(referencedObject.dataSource, options, onData, $scope.onError);
+    
+    function onData(data) {
+        console.log(data);
+        const records = data.records;
+        const types = data.types;
+        const fields = data.viewParams.fields;
+        $scope.title = data.viewParams.tableLabel;
+        
+        const header = [];
+        for(var i = 0; i < showFields.length; i++) {
+            const field = fields[showFields[i]];
+            header.push( field.label );
+        }
+        $scope.header = header;
+        
+        const showRecords = [];
+        for(var i = 0; i < records.length; i++) {
+            const recordValues = [];
+            const item = {};
+            const record = records[i];
+            for(var j = 0; j < showFields.length; j++) {
+                const gridField = showFields[j];
+                recordValues.push(record[gridField]);
+            }
+            item.id = record.id;
+            // considering always the first column as the link field
+            item.linkField = recordValues[0];
+            recordValues.splice(0, 1);
+            item.recordValues = recordValues;
+            showRecords.push(item);
+        }
+        $scope.records = showRecords;
+    }
+}
+
+
+var app = angular.module('FilteredObjectsDirective', []);
+app.directive('filteredObjects', function() {
     return {
         restrict: 'E',
         scope: {
@@ -9,50 +57,7 @@ app.directive('filteredObjects', ['server', function(server) {
             id: '=ngId',
             onError: '=ngOnError'
         },
-        controller: ['$scope', function($scope) {
-            const referencedObject = $scope.referencedObject;
-            const filterField = referencedObject.filterField;
-            const showFields = referencedObject.showFields;
-            const filter = {};
-            filter[filterField] = $scope.id;
-            $scope.dataSource = referencedObject.dataSource;
-            const options = { 
-                filters: filter,
-                fieldNames: showFields
-             };
-            server.getTable(referencedObject.dataSource, options, onData, $scope.onError);
-            
-            function onData(data) {
-                const records = data.records;
-                const fields = data.viewParams.fields;
-                $scope.title = data.viewParams.tableLabel;
-                
-                const header = [];
-                for(var i = 0; i < showFields.length; i++) {
-                    const field = fields[showFields[i]];
-                    header.push( field.label );
-                }
-                $scope.header = header;
-                
-                const showRecords = [];
-                for(var i = 0; i < records.length; i++) {
-                    const recordValues = [];
-                    const item = {};
-                    const record = records[i];
-                    for(var j = 0; j < showFields.length; j++) {
-                        const gridField = showFields[j];
-                        recordValues.push(record[gridField]);
-                    }
-                    item.id = record.id;
-                    // considering always the first column as the link field
-                    item.linkField = recordValues[0];
-                    recordValues.splice(0, 1);
-                    item.recordValues = recordValues;
-                    showRecords.push(item);
-                }
-                $scope.records = showRecords;
-            }
-        }],
+        controller: ['$scope', 'server', controllerFunc],
        templateUrl: 'app/directives/templates/filtered_objects.html'
     };
-}]);
+});
