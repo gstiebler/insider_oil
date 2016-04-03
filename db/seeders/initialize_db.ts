a2bf2d2c460093a82831795943076f80b9267c66
Guilherme Stiebler <guilhermemst@gmail.com>
2016-04-03 10:42:13 -0300

    Fix initialize db

--- db/seeders/initialize_db.ts
+++ db/seeders/initialize_db.ts
@@ -1,8 +1,8 @@
-var db = require( '../models' );
+import db = require( '../models' );
 var umzug = require('../../lib/InitUmzug');
 var Sync = require('sync');
-var winston = require('winston');
-var createFixtures = require('../../test/fixtures/initial_data');
+import winston = require('winston');
+import createFixtures = require('../../test/fixtures/initial_data');
 var await = require('../../lib/await');
 
 winston.add(winston.transports.File, { filename: 'log/seeder.log' });
@@ -11,7 +11,7 @@ Sync(function() {
     try {
         await( db.sequelize.getQueryInterface().dropAllTables() );
         await( umzug.up() );
-        createFixtures();
+        createFixtures.createFixtures();
     } catch(e) {
         winston.error(e.errors);
         winston.error(e.stack);
