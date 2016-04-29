929572ab0367cb61f06441bf0bf5546f647e6b70
Guilherme Stiebler <guilhermemst@gmail.com>
2016-04-29 17:02:59 -0300

    fixing things

--- lib/excel/importExcel.ts
+++ lib/excel/importExcel.ts
@@ -4,7 +4,7 @@ import { ImportExcel, IOkFunc } from './ImportExcelClass';
 import { BlockC } from './Block';
    
 
-export function execute(excelBuf, modelName: string, onOk?, onError?) {
+function execute(excelBuf, modelName: string, onOk?, onError?) {
     var importExcel:ImportExcel;
     if(modelName == 'Block') {
         importExcel = new BlockC();
@@ -13,3 +13,5 @@ export function execute(excelBuf, modelName: string, onOk?, onError?) {
     }
     importExcel.execute(excelBuf, modelName, onOk, onError);
 }
+
+export = execute;
--- test/excel.ts
+++ test/excel.ts
@@ -28,7 +28,7 @@ importDrillingRigOffshore: function(test: nodeunit.Test) {
     test.equal( fixtureCount, await( db.models.DrillingRigOffshore.findAll() ).length );  
     var excelBuf = fs.readFileSync('./test/data/drilling_rigs.xls');
     try {
-        importExcel.execute(excelBuf, 'DrillingRigOffshore', onImportDone, onError);
+        importExcel(excelBuf, 'DrillingRigOffshore', onImportDone, onError);
     } catch(err) {
         console.log(err);
     }
@@ -110,7 +110,7 @@ importBlocks: test => {
     var fixtureCount = 3;
     test.equal( fixtureCount, await( db.models.Block.findAll() ).length );  
     var excelBuf = fs.readFileSync('./test/data/blocks.xlsx');
-    importExcel.execute(excelBuf, 'Block', onImportDone, onError);
+    importExcel(excelBuf, 'Block', onImportDone, onError);
     
     function onImportDone(status, invalidRecordsStatus) {
         var rows = await( dbUtils.findAllCustom(db.models.Block));
@@ -233,7 +233,7 @@ invalidHeader: function(test) {
     var modifiedExcelBuf = XLSX.write(workbook, {type:"buffer"});
     
     try {
-        importExcel.execute(modifiedExcelBuf, 'DrillingRigOffshore');
+        importExcel(modifiedExcelBuf, 'DrillingRigOffshore');
     } catch(err) {
         test.equal( "O cabeçalho do arquivo Excel não possui o campo sonda", err );
         test.done();
