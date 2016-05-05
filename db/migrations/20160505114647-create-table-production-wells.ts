729dcb55624d99f8ffadaf76936763ffb5f3412f
Guilherme Stiebler <guilhermemst@gmail.com>
2016-05-05 11:55:23 -0300

    Production well migration

---
+++ db/migrations/20160505114647-create-table-production-wells.ts
@@ -0,0 +1,53 @@
+'use strict';
+import Sequelize = require('sequelize');
+
+module.exports = {
+  up: function (queryInterface: Sequelize.QueryInterface, SequelizeVar: Sequelize.Sequelize) {
+    const tableOpts: Sequelize.DefineAttributes = {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      operator_name: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      oil_field_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'oil_fields',
+          key: 'id'
+        }
+      },
+      production_unit_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'production_units',
+          key: 'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    };
+
+    queryInterface.createTable('production_wells', tableOpts);
+  },
+
+  down: function (queryInterface: Sequelize.QueryInterface, SequelizeVar: Sequelize.Sequelize) {
+    return queryInterface.dropTable('production_wells');
+  }
+};
