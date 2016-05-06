9d7cbd7056449d6fb8a32676de1adb9e32a9b80b
Guilherme Stiebler <guilhermemst@gmail.com>
2016-05-06 11:44:23 -0300

    Production test

--- test/ProductionQueriesTest.ts
+++ test/ProductionQueriesTest.ts
@@ -19,8 +19,17 @@ byField:  (test: nodeunit.Test) => {
         }
     };
     const resQueryValues = utils.getJsonResponse.sync(null, dbServerController.getProduction, reqQueryValues);
-    console.log(resQueryValues.records);
+    //console.log(resQueryValues.records);
     test.equal(4, resQueryValues.records.length);
+    test.equal( '07/2014', resQueryValues.records[0].date_prod);
+    test.equal( '08/2014', resQueryValues.records[1].date_prod);
+    test.equal( '07/2015', resQueryValues.records[2].date_prod);
+    test.equal( '08/2015', resQueryValues.records[3].date_prod);
+    
+    test.equal( 3.213399887084961, resQueryValues.records[0].oil_production);
+    test.equal( 0.9700000286102295, resQueryValues.records[0].oil_condensed_production);
+    test.equal( 189484, resQueryValues.records[0].gas_associated_production);
+    test.equal( 487, resQueryValues.records[0].gas_non_associated_production);
 
     test.done();
 },
