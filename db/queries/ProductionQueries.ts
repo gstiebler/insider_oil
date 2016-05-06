b7733f1f9e14acc1a7019d9390935362cee6582c
Guilherme Stiebler <guilhermemst@gmail.com>
2016-05-06 11:39:15 -0300

    more fields to the query

--- db/queries/ProductionQueries.ts
+++ db/queries/ProductionQueries.ts
@@ -4,7 +4,11 @@ import db = require('../models');
 
 const queries = {
     byField: (queryParams) => {
-        const select = 'select sum(oil_production) as oil_production, count(p.id), concat(lpad(period_month, 2, "0"), "/", period_year) as date_prod ';
+        const select = 'select sum(oil_production) as oil_production, ' +
+            'sum(oil_condensed_production) as oil_condensed_production, ' +
+            'sum(gas_associated_production) as gas_associated_production, ' +
+            'sum(gas_non_associated_production) as gas_non_associated_production, ' +
+            'concat(lpad(period_month, 2, "0"), "/", period_year) as date_prod ';
         const from = 'from production p ';
 	    const pwJoin = 'left outer join production_wells pw on p.production_well_id = pw.id ';
         const ofJoin = 'left outer join oil_fields of on pw.oil_field_id = of.id ';
