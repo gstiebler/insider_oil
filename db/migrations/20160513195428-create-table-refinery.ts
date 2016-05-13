82bfe5d975f048a8b98780b9ee15e22b3f2691e4
Guilherme Stiebler <guilhermemst@gmail.com>
2016-05-13 19:57:27 -0300

    Refinery migration

---
+++ db/migrations/20160513195428-create-table-refinery.ts
@@ -0,0 +1,49 @@
+'use strict';
+import Sequelize = require('sequelize');
+
+module.exports = {
+  up: function (queryInterface: Sequelize.QueryInterface, SequelizeVar: Sequelize.Sequelize) {
+    const tableOpts: Sequelize.DefineAttributes = {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      telephones_text: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      capacity: {
+        type: Sequelize.FLOAT,
+        allowNull: true
+      },
+      info: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    };
+
+    queryInterface.createTable('refineries', tableOpts);
+  },
+
+  down: function (queryInterface: Sequelize.QueryInterface, SequelizeVar: Sequelize.Sequelize) {
+    return queryInterface.dropTable('refineries');
+  }
+};
