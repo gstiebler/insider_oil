5570298cb1d9307750a1af85161cf864efe17ce3
Guilherme Stiebler <guilhermemst@gmail.com>
2016-05-14 13:09:31 -0300

    Terminals migration

---
+++ db/migrations/20160514130713-create-table-terminals.ts
@@ -0,0 +1,41 @@
+'use strict';
+import Sequelize = require('sequelize');
+
+module.exports = {
+  up: function (queryInterface: Sequelize.QueryInterface, SequelizeVar: Sequelize.Sequelize) {
+    const tableOpts: Sequelize.DefineAttributes = {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      info: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    };
+
+    queryInterface.createTable('terminals', tableOpts);
+  },
+
+  down: function (queryInterface: Sequelize.QueryInterface, SequelizeVar: Sequelize.Sequelize) {
+    return queryInterface.dropTable('terminals');
+  }
+};
