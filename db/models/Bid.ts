cd5080fbc7e0caa6c190a09cef760746227ed8b6
Guilherme Stiebler <guilhermemst@gmail.com>
2016-05-16 14:08:54 -0300

    Recovering referenced object from bid

--- db/models/Bid.ts
+++ db/models/Bid.ts
@@ -1,5 +1,6 @@
 'use strict';
 import Sequelize = require('sequelize');
+var await = require('../../lib/await');
 
 function updateBid(db, bid) {
     const object:any[] = bid.dataValues.object;
@@ -66,7 +67,15 @@ module.exports = function (sequelize, DataTypes: Sequelize.DataTypes) {
         object: {
             type: DataTypes.VIRTUAL,
             get: function() {
-                return '';
+                const modelRecord = await( sequelize.models.ModelsList.findById(this.model_id) );
+                const referencedModel = sequelize.models[modelRecord.name];
+                const referencedObj = await( referencedModel.findById(this.obj_id) );
+                return [{
+                    id: this.obj_id,
+                    model_id: this.model_id,
+                    model: modelRecord.name,
+                    name: referencedObj.name
+                }];
             }
         },
     },
