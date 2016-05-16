fde0a2df422173af3758c7dcba7650edfa9bf096
Guilherme Stiebler <guilhermemst@gmail.com>
2016-05-16 10:54:40 -0300

    Bids migration

---
+++ db/migrations/20160516103346-create-table-bids.ts
@@ -0,0 +1,77 @@
+'use strict';
+import Sequelize = require('sequelize');
+
+module.exports = {
+  up: function (queryInterface: Sequelize.QueryInterface, SequelizeVar: Sequelize.Sequelize) {
+    const tableOpts: Sequelize.DefineAttributes = {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      process_number: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      modality: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      contract_object: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      qty_items: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      opening_moment: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      opening_local: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      opening_city: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      opening_state: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      situation: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      model_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'models_list',
+          key: 'id'
+        }
+      },
+      obj_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    };
+
+    queryInterface.createTable('bids', tableOpts);
+  },
+
+  down: function (queryInterface: Sequelize.QueryInterface, SequelizeVar: Sequelize.Sequelize) {
+    return queryInterface.dropTable('bids');
+  }
+};
