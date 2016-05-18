0a1511cf6aa850c18781852d0d1a219851af60f1
Guilherme Stiebler <guilhermemst@gmail.com>
2016-05-18 14:48:18 -0300

    Oil pipeline migration

---
+++ db/migrations/20160518142433-create-table-oil-pipeline.ts
@@ -0,0 +1,89 @@
+'use strict';
+import Sequelize = require('sequelize');
+
+module.exports = {
+  up: function (queryInterface: Sequelize.QueryInterface, SequelizeVar: Sequelize.Sequelize) {
+    const tableOpts: Sequelize.DefineAttributes = {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      diameter: {
+        type: Sequelize.FLOAT,
+        allowNull: true
+      },
+      extension: {
+        type: Sequelize.FLOAT,
+        allowNull: true
+      },
+      src_state: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      src_location: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      dst_state: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      dst_location: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      products: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      owner_preference: {
+        type: Sequelize.FLOAT,
+        allowNull: true
+      },
+      start_date: {
+        allowNull: true,
+        type: Sequelize.DATEONLY
+      },
+      max_capacity: {
+        type: Sequelize.FLOAT,
+        allowNull: true
+      },
+      op_capacity: {
+        type: Sequelize.FLOAT,
+        allowNull: true
+      },
+      contract_capacity: {
+        type: Sequelize.FLOAT,
+        allowNull: true
+      },
+      contract_released_capacity: {
+        type: Sequelize.FLOAT,
+        allowNull: true
+      },
+      contract_start_date: {
+        allowNull: true,
+        type: Sequelize.DATEONLY
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    };
+
+    queryInterface.createTable('oil_pipelines', tableOpts);
+  },
+
+  down: function (queryInterface: Sequelize.QueryInterface, SequelizeVar: Sequelize.Sequelize) {
+    return queryInterface.dropTable('oil_pipelines');
+  }
+};
