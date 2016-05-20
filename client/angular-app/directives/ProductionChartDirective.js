9b7b25ff8ef1618744c3e4ee0978df3584aa799d
Guilherme Stiebler <guilhermemst@gmail.com>
2016-05-19 21:37:52 -0300

    Changes chart height

--- client/angular-app/directives/ProductionChartDirective.js
+++ client/angular-app/directives/ProductionChartDirective.js
@@ -18,7 +18,7 @@ function showChart(records) {
        
     var margin = {top: 20, right: 20, bottom: 30, left: 40},
         width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        height = 250 - margin.top - margin.bottom;
         
     var parseDate = d3.time.format("%m/%Y").parse;
 
