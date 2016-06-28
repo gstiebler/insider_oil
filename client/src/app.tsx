d59f8344e1b77d01e148a821f5235183ed0a340a
Guilherme Stiebler <guilhermemst@gmail.com>
2016-06-28 13:48:54 -0300

    refactoring and css adjustments

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -57,79 +57,111 @@ class InsiderOilApp extends React.Component<IAppProps, IAppState> {
   }
 
   public render() {
-    var adminLink = <li class="nav-item" >
+    var adminLink = <li className="nav-item" >
                       <a href="/app/"><b>Admin</b></a>
                     </li>;
 
-    return (
-      <div class="container-1450">
-        <div className="top-menu">
-          <div className="container-fluid">
-            <div class="row">
-              <div className="col-md-4 col-hidden"></div>
-              <div className="col-no-padding col-md-8">
-                <div className="col-md-3 col-no-padding">
-                  <div className="user-name">
-                    Olá
-                    <span>{this.state.username} | </span>
-                    <a href="#" onClick={ session.logout }>sair</a>
-                  </div>
-                </div>
-                <div className="col-md-3 col-no-padding">
-                  <div className="top-menu-item">
-                    <div className="top-menu-icon window"></div>
-                    Meu Dashboard
-                  </div>
-                </div>
-                <div className="col-md-3 col-no-padding">
-                  <div className="top-menu-item">
-                    <div className="top-menu-icon user"></div>
-                    <a href="/app/change_password">Minha Conta</a>
-                  </div>
-                </div>
-                <div className="col-md-3 col-no-padding">
-                  <div className="top-menu-item">
-                    <div className="top-menu-icon question"></div>
-                    SAC
-                  </div>
-                </div>
-              </div>
-            </div>
+    var userNameMenuItem = (
+        <div className="col-md-3 col-no-padding">
+          <div className="user-name">
+            Olá 
+            <span> {this.state.username} | </span>
+            <a href="#" onClick={ session.logout }>sair</a>
           </div>
         </div>
-        <nav className="navbar navbar-default navbar-border-none" role="navigation">
+    );
+
+    var myDashboardMenuItem = (
+      <div className="col-md-3 col-no-padding">
+        <div className="top-menu-item">
+          <div className="top-menu-icon window"></div>
+          Meu Dashboard
+        </div>
+      </div>
+    );
+
+    var myAccountMenuItem = (
+      <div className="col-md-3 col-no-padding">
+        <div className="top-menu-item">
+          <div className="top-menu-icon user"></div>
+          <a href="/app/change_password">Minha Conta</a>
+        </div>
+      </div>
+    );
+
+    var sacMenuItem = (      
+        <div className="col-md-3 col-no-padding">
+          <div className="top-menu-item">
+            <div className="top-menu-icon question"></div>
+            SAC
+          </div>
+        </div>
+    );
+
+    var menu = (
+      <div className="top-menu">
+          <div className="container-fluid">
+              <div className="row">
+                  <div className="col-md-4 col-hidden"></div>
+                  <div className="col-no-padding col-md-8">
+                    { userNameMenuItem }
+                    { myDashboardMenuItem }
+                    { myAccountMenuItem }
+                    { sacMenuItem }
+                  </div>
+              </div>
+          </div>
+      </div>
+    );
+
+    var logo = (
+      <div className="col-md-4">
+        <div className="navbar-header navbar-brand-container">
+          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand logo-topo" href="/app/" id="homeLink"></a>
+        </div>
+      </div>
+    );
+
+    var navBarItems = (
+        <div className="col-no-padding col-md-8">
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav navbar-nav-full">
+              <li className="active nav-item">
+                <a href="/app/tree?nodeId=1">Dados</a>
+              </li>
+              <li className="nav-item">
+                <a href="/app/tree?nodeLabel=Pessoas">Pessoas</a>
+              </li>
+              <li className="nav-item">
+                <a href="/app/tree?nodeLabel=Empresas">Empresas</a>
+              </li>
+              { this.state.isAdmin ? adminLink : '' }
+            </ul>
+          </div>
+        </div>
+    );
+
+    var navBar = (
+      <nav className="navbar navbar-default navbar-border-none" role="navigation">
           <div className="container-fluid padding-lr-20">
             <div className="row">
-              <div className="col-md-4">
-                <div className="navbar-header navbar-brand-container">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                  </button>
-                  <a className="navbar-brand logo-topo" href="/app/" id="homeLink"></a>
-                </div>
-              </div>
-              <div className="col-no-padding col-md-8">
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <ul className="nav navbar-nav navbar-nav-full">
-                    <li className="active nav-item">
-                      <a href="/app/tree?nodeId=1">Dados</a>
-                    </li>
-                    <li className="nav-item">
-                      <a href="/app/tree?nodeLabel=Pessoas">Pessoas</a>
-                    </li>
-                    <li className="nav-item">
-                      <a href="/app/tree?nodeLabel=Empresas">Empresas</a>
-                    </li>
-                    { this.state.isAdmin ? adminLink : '' }
-                  </ul>
-                </div>
-              </div>
+              { logo }
+              { navBarItems }
             </div>
           </div>
         </nav>
+    );
+
+    return (
+      <div className="container-1450">
+        { menu }
+        { navBar }
       </div>
     );
   }
