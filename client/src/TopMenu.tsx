678d7a9d4c7b85798cf97d467a6b545ed8e938fd
Guilherme Stiebler <guilhermemst@gmail.com>
2016-06-28 16:09:05 -0300

    Component for top menu

---
+++ client/src/TopMenu.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import * as session from './session';
+
+interface IAppProps {
+    username: string;
+}
+
+interface IAppState {
+}
+
+export class TopMenu extends React.Component<IAppProps, IAppState> {
+
+    public render():JSX.Element {
+
+        var userNameMenuItem = (
+            <div className="col-md-3 col-no-padding">
+            <div className="user-name">
+                Olá 
+                <span> {this.props.username} | </span>
+                <a href="#" onClick={ session.logout }>sair</a>
+            </div>
+            </div>
+        );
+
+        var myDashboardMenuItem = (
+        <div className="col-md-3 col-no-padding">
+            <div className="top-menu-item">
+            <div className="top-menu-icon window"></div>
+            Meu Dashboard
+            </div>
+        </div>
+        );
+
+        var myAccountMenuItem = (
+        <div className="col-md-3 col-no-padding">
+            <div className="top-menu-item">
+            <div className="top-menu-icon user"></div>
+            <a href="/app/change_password">Minha Conta</a>
+            </div>
+        </div>
+        );
+
+        var sacMenuItem = (      
+            <div className="col-md-3 col-no-padding">
+            <div className="top-menu-item">
+                <div className="top-menu-icon question"></div>
+                SAC
+            </div>
+            </div>
+        );
+
+        var menu = (
+            <div className="top-menu">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-4 col-hidden"></div>
+                        <div className="col-no-padding col-md-8">
+                            { userNameMenuItem }
+                            { myDashboardMenuItem }
+                            { myAccountMenuItem }
+                            { sacMenuItem }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+
+        return menu;
+    }
+}
--- client/src/app.tsx
+++ client/src/app.tsx
@@ -3,9 +3,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
-import * as session from './session';
 import * as server from './Server';
 import { AdminList } from './AdminList';
+import { TopMenu } from './TopMenu'
+import * as session from './session';
 
 interface IAppProps {
   model: string;
@@ -13,8 +14,8 @@ interface IAppProps {
 }
 
 interface IAppState {
-  username: string;
-  isAdmin: boolean;
+    username: string;
+    isAdmin: boolean;
 }
 
 
@@ -26,16 +27,14 @@ class InsiderOilApp extends React.Component<IAppProps, IAppState> {
     super(props);
 
     this.state = {
-        username: '',
-        isAdmin: false
+        isAdmin: false,
+        username: ''
     };
 
     var token = props.location.query.token;
     if( token ) {
         session.login( token );
-    } else {
-        token = session.getToken();
-    }    
+    }  
   
     server.getUserDetails(function(response) {
        this.setState({
@@ -59,58 +58,6 @@ class InsiderOilApp extends React.Component<IAppProps, IAppState> {
                       <Link to="/app/admin"><b>Admin</b></Link>
                     </li>;
 
-    var userNameMenuItem = (
-        <div className="col-md-3 col-no-padding">
-          <div className="user-name">
-            Olá 
-            <span> {this.state.username} | </span>
-            <a href="#" onClick={ session.logout }>sair</a>
-          </div>
-        </div>
-    );
-
-    var myDashboardMenuItem = (
-      <div className="col-md-3 col-no-padding">
-        <div className="top-menu-item">
-          <div className="top-menu-icon window"></div>
-          Meu Dashboard
-        </div>
-      </div>
-    );
-
-    var myAccountMenuItem = (
-      <div className="col-md-3 col-no-padding">
-        <div className="top-menu-item">
-          <div className="top-menu-icon user"></div>
-          <a href="/app/change_password">Minha Conta</a>
-        </div>
-      </div>
-    );
-
-    var sacMenuItem = (      
-        <div className="col-md-3 col-no-padding">
-          <div className="top-menu-item">
-            <div className="top-menu-icon question"></div>
-            SAC
-          </div>
-        </div>
-    );
-
-    var menu = (
-      <div className="top-menu">
-          <div className="container-fluid">
-              <div className="row">
-                  <div className="col-md-4 col-hidden"></div>
-                  <div className="col-no-padding col-md-8">
-                    { userNameMenuItem }
-                    { myDashboardMenuItem }
-                    { myAccountMenuItem }
-                    { sacMenuItem }
-                  </div>
-              </div>
-          </div>
-      </div>
-    );
 
     var logo = (
       <div className="col-md-4">
@@ -159,7 +106,7 @@ class InsiderOilApp extends React.Component<IAppProps, IAppState> {
     return (
       <div>
         <div className="container-1450">
-          { menu }
+          <TopMenu username={this.state.username} ></TopMenu>
           { navBar }
         </div>
         <div className="container-1450">
