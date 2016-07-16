b6ae519c30b1de15a9829d58c0a433a65f774508
Guilherme Stiebler <guilhermemst@gmail.com>
2016-07-16 17:33:48 -0300

    Refactoring on KML import

--- server/lib/ImportKml.ts
+++ server/lib/ImportKml.ts
@@ -19,155 +19,88 @@ function getSimpleData(placemark) {
     return placemark.ExtendedData[0].SchemaData[0].SimpleData;
 }
 
-export function importBlocks(kmlStr:string):Promise<string> {
-
-    function getRegions(placemark, BlockName:string):any[] {
-        var kmlPolygons = [];
-        if(placemark.Polygon) {
-            const linearRing = placemark.Polygon[0].outerBoundaryIs[0].LinearRing[0];
-            kmlPolygons.push(linearRing);
-        } else if (placemark.MultiGeometry) {
-            kmlPolygons = placemark.MultiGeometry[0].Polygon.map(polygon => {
-                const linearRing = polygon.outerBoundaryIs[0].LinearRing[0];
-                return linearRing;
-            });
-        } else {
-            winston.debug('sem polígonos em ' + BlockName);
-            return;
-        }
-
-        const regions = [];
-        kmlPolygons.map(linearRing => {
-            const coordinatesRawStr:string = linearRing.coordinates[0];
-            const coordinatesStr = coordinatesRawStr.replace(/\n/g, '').replace(/\t/g, '');
-
-            const polygons = [];
-            const coordinateStrs = coordinatesStr.split(' ');
-            const coordinates = coordinateStrs.map((floatStr:string) => {
-                const floatStrs = floatStr.split(',');
-                if(floatStrs.length != 3) {
-                    return;
-                }
-                polygons.push({
-                    lat: parseFloat(floatStrs[1]),
-                    lng: parseFloat(floatStrs[0])
-                });
-            });
-
-            regions.push(polygons);
+function getRegions(placemark, objName:string):any[] {
+    var kmlPolygons = [];
+    if(placemark.Polygon) {
+        const linearRing = placemark.Polygon[0].outerBoundaryIs[0].LinearRing[0];
+        kmlPolygons.push(linearRing);
+    } else if (placemark.MultiGeometry) {
+        kmlPolygons = placemark.MultiGeometry[0].Polygon.map(polygon => {
+            const linearRing = polygon.outerBoundaryIs[0].LinearRing[0];
+            return linearRing;
         });
-        return regions;
+    } else {
+        winston.debug('sem polígonos em ' + objName);
+        return;
     }
 
-    function processBlockKml(kmlObj:any):string {
-        const missingBlocks:string[] = [];
-        var okBlocks = 0;
-        const Placemarks = getPlacemarks(kmlObj);
-        Placemarks.map(placemark => {
-            const simpleData = getSimpleData(placemark);
-            const BlockName = simpleData[0]._;
-            const block = await( db.models.Block.findOne({ where: { name: BlockName } }) );
-            if(!block) {
-                missingBlocks.push(BlockName);
+    const regions = [];
+    kmlPolygons.map(linearRing => {
+        const coordinatesRawStr:string = linearRing.coordinates[0];
+        const coordinatesStr = coordinatesRawStr.replace(/\n/g, '').replace(/\t/g, '');
+
+        const polygons = [];
+        const coordinateStrs = coordinatesStr.split(' ');
+        const coordinates = coordinateStrs.map((floatStr:string) => {
+            const floatStrs = floatStr.split(',');
+            if(floatStrs.length != 3) {
                 return;
             }
-            
-            const regions = getRegions(placemark, BlockName);
-            block.polygons = JSON.stringify(regions);
-            await( block.save() );
-            okBlocks++;
+            polygons.push({
+                lat: parseFloat(floatStrs[1]),
+                lng: parseFloat(floatStrs[0])
+            });
         });
-        return okBlocks + ' importados com sucesso. Os seguintes blocos não foram encontrados: ' + missingBlocks.join('\n');
-    }
 
+        regions.push(polygons);
+    });
+    return regions;
+}
+
+function processKmlObj(kmlObj:any, modelName: string, nameIndex: number):string {
+    const missingObjs:string[] = [];
+    var okObjs = 0;
+    const model = db.models[modelName];
+    const Placemarks = getPlacemarks(kmlObj);
+    Placemarks.map(placemark => {
+        const simpleData = getSimpleData(placemark);
+        const objName:string = simpleData[nameIndex]._;
+        const withoutAccents = removerAcentos(objName);
+        const withoutSpecialCharacters = withoutAccents.replace(/[^\w\s]/gi, '')
+        winston.info('resultado remoçÃo acentos: ', withoutSpecialCharacters);
+        const objOnDB = await( model.findOne({ where: { name: withoutSpecialCharacters } }) );
+        if(!objOnDB) {
+            missingObjs.push(objName);
+            return; 
+        }
+        
+        const regions = getRegions(placemark, objName);
+        objOnDB.polygons = JSON.stringify(regions);
+        await( objOnDB.save() );
+        okObjs++;
+    });
+    return okObjs + ' importados com sucesso. Os seguintes blocos não foram encontrados: ' + missingObjs.join('\n');
+}
+
+function processKmlString(kmlStr: string, modelName: string, nameIndex: number):Promise<string> {
     return new Promise((resolve, reject) => {
         parseString(kmlStr, function (err, result) { Sync(function() {
             if(result) {
-                const status = processBlockKml(result);
+                const status = processKmlObj(result, modelName, nameIndex);
                 resolve(status);
             } else if(err) {
                 reject(err);
             } else {
-                reject('Erro ao importar bloco');
+                reject('Erro ao importar kml');
             }
         })});
     });
 }
 
+export function importBlocks(kmlStr:string):Promise<string> {
+    return processKmlString(kmlStr, 'Block', 0);
+}
+
 export function importOilFields(kmlStr:string):Promise<string> {
-
-    function getRegions(placemark, OilFieldName:string):any[] {
-        var kmlPolygons = [];
-        if(placemark.Polygon) {
-            const linearRing = placemark.Polygon[0].outerBoundaryIs[0].LinearRing[0];
-            kmlPolygons.push(linearRing);
-        } else if (placemark.MultiGeometry) {
-            kmlPolygons = placemark.MultiGeometry[0].Polygon.map(polygon => {
-                const linearRing = polygon.outerBoundaryIs[0].LinearRing[0];
-                return linearRing;
-            });
-        } else {
-            winston.debug('sem polígonos em ' + OilFieldName);
-            return;
-        }
-
-        const regions = [];
-        kmlPolygons.map(linearRing => {
-            const coordinatesRawStr:string = linearRing.coordinates[0];
-            const coordinatesStr = coordinatesRawStr.replace(/\n/g, '').replace(/\t/g, '');
-
-            const polygons = [];
-            const coordinateStrs = coordinatesStr.split(' ');
-            const coordinates = coordinateStrs.map((floatStr:string) => {
-                const floatStrs = floatStr.split(',');
-                if(floatStrs.length != 3) {
-                    return;
-                }
-                polygons.push({
-                    lat: parseFloat(floatStrs[1]),
-                    lng: parseFloat(floatStrs[0])
-                });
-            });
-
-            regions.push(polygons);
-        });
-        return regions;
-    }
-
-    function processOilFieldKml(kmlObj:any):string {
-        const missingOilFields:string[] = [];
-        var okOilFields = 0;
-        const Placemarks = getPlacemarks(kmlObj);
-        Placemarks.map(placemark => {
-            const simpleData = getSimpleData(placemark);
-            const OilFieldName:string = simpleData[1]._;
-            const withoutAccents = removerAcentos(OilFieldName);
-            const withoutSpecialCharacters = withoutAccents.replace(/[^\w\s]/gi, '')
-			winston.info('resultado remoçÃo acentos: ', withoutSpecialCharacters);
-            const oilField = await( db.models.OilField.findOne({ where: { name: withoutSpecialCharacters } }) );
-            if(!oilField) {
-                missingOilFields.push(OilFieldName);
-                return; 
-            }
-            
-            const regions = getRegions(placemark, OilFieldName);
-            oilField.polygons = JSON.stringify(regions);
-            await( oilField.save() );
-            okOilFields++;
-        });
-        return okOilFields + ' importados com sucesso. Os seguintes blocos não foram encontrados: ' + missingOilFields.join('\n');
-    }
-
-    return new Promise((resolve, reject) => {
-        parseString(kmlStr, function (err, result) { Sync(function() {
-            if(result) {
-                const status = processOilFieldKml(result);
-                resolve(status);
-            } else if(err) {
-                reject(err);
-            } else {
-                reject('Erro ao importar bloco');
-            }
-        })});
-    });
+    return processKmlString(kmlStr, 'OilField', 1);
 }
