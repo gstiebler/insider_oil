b3b50c74baed5420acff4c9726ec050aeb432dcd
Guilherme Stiebler <guilhermemst@gmail.com>
2016-07-18 22:22:59 -0300

    Error report Bootstrap style

--- client/src/ErrorReport.tsx
+++ client/src/ErrorReport.tsx
@@ -19,31 +19,41 @@ export class ErrorReport extends React.Component<IAppProps, IAppState> {
     }
 
     private closeModal() {
-        this.setState({modalIsOpen: false});
+        this.setState({ modalIsOpen: false });
     }
 
-    
     private openModal() {
-        this.setState({modalIsOpen: true});
+        this.setState({ modalIsOpen: true });
     }
 
     public render(): React.ReactElement<any> {
         return (
             <div>
-                <button onClick={this.openModal.bind(this)}>Open Modal</button>
+                <button onClick={this.openModal.bind(this) }>Open Modal</button>
                 <Modal
-                    isOpen={this.state.modalIsOpen}>
-
-                    <h2 ref="subtitle">Hello</h2>
-                    <button onClick={this.closeModal.bind(this)}>close</button>
-                    <div>I am a modal</div>
-                    <form>
-                        <input />
-                        <button>tab navigation</button>
-                        <button>stays</button>
-                        <button>inside</button>
-                        <button>the modal</button>
-                    </form>
+                    className="Modal__Bootstrap modal-dialog"
+                    closeTimeoutMS={150}
+                    isOpen={this.state.modalIsOpen}
+                    >
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button type="button" className="close" onClick={this.closeModal.bind(this)}>
+                                <span aria-hidden="true">&times; </span>
+                                <span className="sr-only">Close</span>
+                            </button>
+                            <h4 className="modal-title">Modal title</h4>
+                        </div>
+                        <div className="modal-body">
+                            <h4>Really long content...</h4>
+                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.Donec eu libero sit amet quam egestas semper.Aenean ultricies mi vitae est.Mauris placerat eleifend leo.Quisque sit amet est et sapien ullamcorper pharetra.Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.Donec non enim in turpis pulvinar facilisis.Ut felis.Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.Aliquam erat volutpat.Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.Donec eu libero sit amet quam egestas semper.Aenean ultricies mi vitae est.Mauris placerat eleifend leo.Quisque sit amet est et sapien ullamcorper pharetra.Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.Donec non enim in turpis pulvinar facilisis.Ut felis.Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.Aliquam erat volutpat.Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.Donec eu libero sit amet quam egestas semper.Aenean ultricies mi vitae est.Mauris placerat eleifend leo.Quisque sit amet est et sapien ullamcorper pharetra.Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.Donec non enim in turpis pulvinar facilisis.Ut felis.Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.Aliquam erat volutpat.Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" onClick={this.closeModal.bind(this)} >Close</button>
+                            <button type="button" className="btn btn-primary" >Save changes</button>
+                        </div>
+                    </div>
                 </Modal>
             </div>
         );
