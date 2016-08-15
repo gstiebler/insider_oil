58efb42afb673c7293b7cf1ac8af3e59c607264e
Guilherme Stiebler <guilhermemst@gmail.com>
2016-08-15 11:56:53 -0300

    Adding SuperTrump to show companies in People list

---
+++ client/src/SuperTrump.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+interface IAppProps {
+}
+
+interface IAppState {
+}
+
+export class SuperTrump extends React.Component<IAppProps, IAppState> {
+
+    constructor(props: IAppProps) {
+        super(props);
+
+        this.state = {};
+    }
+
+    public render(): React.ReactElement<any> {		
+		return (
+            <div></div> 
+		);
+    }
+}
