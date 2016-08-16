7776b8ecea8ae7d89c280d63690cfaeb803b5c01
Guilherme Stiebler <guilhermemst@gmail.com>
2016-08-16 12:36:41 -0300

    Lazy loading of companies cards on persons page

--- client/src/PersonsByCompany.tsx
+++ client/src/PersonsByCompany.tsx
@@ -13,34 +13,63 @@ interface IAppState {
 
 export class PersonsByCompany extends React.Component<IAppProps, IAppState> {
 
+    private itemsPerPage: number;
+    private lastItem: number;
+    private waitingData: boolean;
+    private count: number;
+
     constructor(props: IAppProps) {
         super(props);
-
+        this.itemsPerPage = 12;
+        this.lastItem = 0;
+        this.waitingData = false;
+        this.count = 9999999;
         this.state = {
             records: []
         };
     }
 
     public componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll.bind(this));
+        this.getData();
+    }
+
+    private getData() {
+        if(this.waitingData || (this.lastItem >= this.count)) {
+            return;
+        }
         const req: ni.GetTableQueryData.req = {
             queryName: 'companyCards',
             queryParams: {
                 order: [], 
                 filters: [],
                 pagination: {
-                    first: 0,
-                    itemsPerPage: 999
+                    first: this.lastItem,
+                    itemsPerPage: this.itemsPerPage
                 }
             }
         }
+        this.waitingData = true;
         getP('/get_table_data', req)
             .then(this.onData.bind(this))
             .catch(showError.show);
     }
 
+    private handleScroll(event) {
+        const heightTreshold = 1000;
+        const diff = event.srcElement.body.scrollHeight - 
+            (event.srcElement.body.scrollTop + window.innerHeight);
+        if(diff < heightTreshold) {
+            this.getData();
+        }
+    }
+
     public onData(data:ni.GetTableQueryData.res) {
-        this.state.records = data.records;
+        this.state.records = this.state.records.concat(data.records);
+        this.count = data.count;
         this.setState(this.state);
+        this.lastItem = this.state.records.length;
+        this.waitingData = false;
         return null;
     }
 
