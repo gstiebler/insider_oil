251a611d27957ab8e1931260310a6875b9dd7853
Guilherme Stiebler <guilhermemst@gmail.com>
2016-08-23 10:56:42 -0300

    Fix name of error log table

--- server/db/models/ErrorLog.ts
+++ server/db/models/ErrorLog.ts
@@ -11,7 +11,7 @@ module.exports = function(sequelize, DataTypes:Sequelize.DataTypes) {
         }, 
         {
             underscored: true,
-            tableName: 'ErrorLog'
+            tableName: 'error_log'
         }
     );
     
