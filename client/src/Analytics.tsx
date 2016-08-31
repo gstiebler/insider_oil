29784abcfa99125027c9a5a36043594700ed7fb9
Guilherme Stiebler <guilhermemst@gmail.com>
2016-08-31 13:00:04 -0300

    Getting analytics sources from server

--- client/src/Analytics.tsx
+++ client/src/Analytics.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import * as server from './lib/Server';
+import * as showError from './lib/ShowError';
+import * as ni from '../../common/NetworkInterfaces';
 
 interface IAppProps {
 }
@@ -16,8 +19,15 @@ export class Analytics extends React.Component<IAppProps, IAppState> {
     }
 
     private componentDidMount() {
+        const req:ni.AnalyticsSources.req = {};
+        server.getP('/analytics/sources', req)
+            .then(this.onData.bind(this))
+            .catch(showError.show);
     }    
 
+    private onData(res: ni.AnalyticsSources.res) {
+        console.log(res);
+    }
 
     public render(): React.ReactElement<any> {		
 		return (
