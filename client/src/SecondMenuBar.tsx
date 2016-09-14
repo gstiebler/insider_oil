1bb17ff2edd54f91ba7e004f1698fff873b8a5ac
Guilherme Stiebler <guilhermemst@gmail.com>
2016-09-14 08:41:06 -0300

    Adding Boat to main menu

--- client/src/SecondMenuBar.tsx
+++ client/src/SecondMenuBar.tsx
@@ -79,6 +79,7 @@ export class SecondMenuBar extends React.Component<IAppProps, IAppState> {
                 <li><Link to="/app/paginated_table_view?source=Bids">Licitações</Link></li>
                 <li><Link to="/app/paginated_table_view?source=MaintenanceContracts">Manutenção, Construção e Montagem Offshore</Link></li>
                 <li><Link to="/app/paginated_table_view?source=Shipyards">Estaleiros</Link></li>
+                <li><Link to="/app/paginated_table_view?source=Boats">Barcos de apoio</Link></li>
               </ul>
             </li>
             <li className="nav-item">
