41c3ce95dcf5f1db0698c90e95037ab0b8152faf
Guilherme Stiebler <guilhermemst@gmail.com>
2016-09-22 11:52:19 -0300

    Select author of insight

--- client/src/Admin/NewsEdit.tsx
+++ client/src/Admin/NewsEdit.tsx
@@ -17,7 +17,9 @@ interface IAppState {
     content: string;
     tableauUrl: string;
     id: number;
+    author_id: number;
     image: any;
+    users: any[];
 }
 
 export class NewsEdit extends React.Component<IAppProps, IAppState> {
@@ -33,7 +35,9 @@ export class NewsEdit extends React.Component<IAppProps, IAppState> {
             content: '',
             tableauUrl: null,
             id: props.location.query.id,
-            image: null
+            author_id: null,
+            image: null,
+            users: []
         };
 
 	    this.modelName = 'News';
@@ -50,12 +54,23 @@ export class NewsEdit extends React.Component<IAppProps, IAppState> {
             this.state.mainTitle = "Nova notícia"
             this.setState(this.state);
         }
+
+        server.getP('/combo_values', { model: 'User' })
+            .then(this.onUsers.bind(this))
+            .catch(showError.show);
     }
 
     private onServerData(data) {
         this.state.title = data.values.title;
         this.state.content = data.values.content;
+        this.state.author_id = data.values.author_id;
         this.state.tableauUrl = data.values.tableau_url;
+        console.log(data.values);
+        this.setState(this.state);
+    }
+
+    private onUsers(users:any[]) {
+        this.state.users = users;
         this.setState(this.state);
     }
 
@@ -73,14 +88,10 @@ export class NewsEdit extends React.Component<IAppProps, IAppState> {
     }
 
     private saveItem() {
-        server.getUserDetails(this.onUserDataArrived.bind(this), showError.show);
-	}
-
-    private onUserDataArrived(userData) {
         var itemData:any = {};
         itemData.title = this.state.title;
         itemData.content = this.state.content;
-        itemData.author_id = userData.id;
+        itemData.author_id = this.state.author_id;
         itemData.image = this.state.image;
         itemData.tableau_url = this.state.tableauUrl;
         if(this.state.id) {
@@ -89,7 +100,7 @@ export class NewsEdit extends React.Component<IAppProps, IAppState> {
         } else {
             server.createNewItem( this.modelName, itemData, this.onSave.bind(this), showError.show );
         }
-    }
+	}
 	
 	private onSave(status) {
 	    Flash.create('success', status.msg);
@@ -100,33 +111,65 @@ export class NewsEdit extends React.Component<IAppProps, IAppState> {
         this.state.image = image;
     }
 
+    private onUserChange(e) {
+        this.state.author_id = e.target.value;
+        this.setState(this.state);
+    }
+
     public render(): React.ReactElement<any> {
         const imgPath = server.paths.baseImg + 'insights/img_' + this.state.id + '.jpg';
+
+        const titleHTML = (                    
+            <div className="form-group">
+                <label className="control-label col-sm-2" htmlFor="title_box">Título:</label>
+                <div className="col-sm-10">
+                    <input type="text" className="form-control" 
+                            value={this.state.title}
+                            onChange={(e) => {this.state.title = e.target.value; this.setState(this.state);}} />
+                </div>    
+            </div>
+        );
+
+        const newContentHTML = (
+            <div className="form-group" style={{border: "3px"}}>
+                <label className="control-label col-sm-2" htmlFor="content_box">Notícia:</label>
+                <div className="col-sm-10">
+                    <table className="table table-bordered"><tbody><tr><td>
+                        <ReactQuill theme="snow" 
+                                value={this.state.content}
+                                onChange={(v) => {this.state.content = v;this.setState(this.state);}}/>
+                    </td></tr></tbody></table>   
+                    Busca: <ProjectSearch onItemSelected={this.onProjectSelected.bind(this)} />
+                </div>
+            </div>
+        );
+
+        const usersHTMLOptions = this.state.users.map((user, i) => {
+            return <option value={user.id} key={i}>{user.label}</option>;
+        });
+
+        const authortHTML = (
+            <div className="form-group">
+                <label className="control-label col-sm-2" htmlFor="title_box">Autor:</label>
+                <div className="col-sm-10">                
+                    <select className="form-control" 
+                        value={this.state.author_id}
+                        onChange={this.onUserChange.bind(this)}>
+                        { usersHTMLOptions }
+                </select>
+                </div>    
+            </div>
+        );
+
         return ( <div>
             <h4 className="col-sm-2" >{this.state.mainTitle}</h4>
             <br/><br/><br/><br/>
             <div className="row">   
                 <div className="form-horizontal">
-                    <div className="form-group">
-                        <label className="control-label col-sm-2" htmlFor="title_box">Título:</label>
-                        <div className="col-sm-10">
-                            <input type="text" className="form-control" 
-                                   value={this.state.title}
-                                   onChange={(e) => {this.state.title = e.target.value; this.setState(this.state);}} />
-                        </div>    
-                    </div>
-                    
-                    <div className="form-group" style={{border: "3px"}}>
-                        <label className="control-label col-sm-2" htmlFor="content_box">Notícia:</label>
-                        <div className="col-sm-10">
-                            <table className="table table-bordered"><tbody><tr><td>
-                                <ReactQuill theme="snow" 
-                                        value={this.state.content}
-                                        onChange={(v) => {this.state.content = v;this.setState(this.state);}}/>
-                            </td></tr></tbody></table>   
-                            Busca: <ProjectSearch onItemSelected={this.onProjectSelected.bind(this)} />
-                        </div>
-                    </div>
+                    { titleHTML }
+                    { authortHTML }
+                    { newContentHTML }
+
                     <div className="form-group">
                         <div className="col-sm-offset-2 col-sm-10">
                             <textarea rows="8" cols="100"
