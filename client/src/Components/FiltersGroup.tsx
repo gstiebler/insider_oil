1782e3e5fb2e99ef05636e72edfb2f68f698e945
Guilherme Stiebler <guilhermemst@gmail.com>
2016-10-22 10:24:11 -0200

    Adding FiltersGroup component file

---
+++ client/src/Components/FiltersGroup.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+interface IAppState {
+}
+
+export class FiltersGroup extends React.Component<IAppProps, IAppState> {
+
+    constructor(props: IAppProps) {
+        super(props);
+
+        this.state = {
+        };
+    }
+
+    private componentDidMount() {
+    }
+
+    private componentWillReceiveProps(nextProps:IAppProps) {
+    }
+
+
+    public render(): React.ReactElement<any> {
+
+		return (
+            <div>
+            </div>
+		);
+    }
+}
