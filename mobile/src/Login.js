30623452e515ccb2eddb4e736797fcfe0581ba08
Guilherme <guilhermemst@gmail.com>
2016-11-20 12:57:54 -0200

    Getting login info from login screen

--- mobile/src/Login.js
+++ mobile/src/Login.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import {
     TouchableHighlight,
     Text,
-    View
+    View,
+    TextInput,
+    StyleSheet,
+    Image
 } from 'react-native';
 import { login } from './lib/session';
 
@@ -10,7 +13,11 @@ export class Login extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            username: '',
+            password: '',
+            errorMsg: null
+        };
     }
 
     componentDidMount() {
@@ -19,20 +26,112 @@ export class Login extends Component {
     async onLoginClicked(navigator) {
         console.log('onLoginClicked')
         try {
-            await login('gstiebler', 'aloalo35');
+            await login(this.state.username, this.state.password);
             this.props.onLogin();
         } catch(err) {
             console.log('erro: ', err);
+            this.setState({errorMsg: err});
         }
     }
  
     render() {
         return (
-            <View style={{padding: 100}} >
-                <TouchableHighlight onPress={ this.onLoginClicked.bind(this) } >
-                    <Text>Hello Login Screen!</Text>
+            <View style={styles.container}>
+                <Image style={styles.mark} source={{uri: 'http://app.insideroil.com/images/logo.jpg'}} />
+                        <TextInput 
+                            style={[styles.input]}
+                            placeholder="Usuário"
+                            value={this.state.username}
+                            onChangeText={(username) => this.setState({username})}
+                        />
+                        <TextInput
+                            password={true}
+                            style={[styles.input]}
+                            placeholder="Senha"
+                            value={this.state.password}
+                            onChangeText={(password) => this.setState({password})}
+                        />
+                <TouchableHighlight 
+                        onPress={ this.onLoginClicked.bind(this) }  
+                        style={styles.signin}>
+                    <Text style={styles.whiteFont}>Login</Text>
                 </TouchableHighlight>
+                <Text style={{margin: 20}} >{this.state.errorMsg}</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'column',
+      flex: 1,
+      //backgroundColor: 'transparent'
+    },
+    bg: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        width: 400, // TODO get window size
+        height: 600 // TODO get window size
+    },
+    header: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: .5,
+        backgroundColor: 'transparent'
+    },
+    mark: {
+        width: 264,
+        height: 84,
+        marginTop: 50,
+        marginLeft: 20
+    },
+    signin: {
+        backgroundColor: '#8EDB82',
+        padding: 20,
+        alignItems: 'center'
+    },
+    signup: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: .15
+    },
+    inputs: {
+        marginTop: 10,
+        marginBottom: 10,
+        flex: .25
+    },
+    inputPassword: {
+        marginLeft: 15,
+        width: 20,
+        height: 21
+    },
+    inputUsername: {
+      marginLeft: 15,
+      width: 20,
+      height: 20
+    },
+    inputContainer: {
+        padding: 10,
+        borderWidth: 1,
+        borderBottomColor: '#CCC',
+        borderColor: 'transparent'
+    },
+    input: {
+        alignSelf: 'stretch',
+        height: 50,
+        fontSize: 14,
+        margin: 15,
+    },
+    forgotContainer: {
+      alignItems: 'flex-end',
+      padding: 15,
+    },
+    greyFont: {
+      color: '#D8D8D8'
+    },
+    whiteFont: {
+      color: '#FFF'
+    }
+})
--- mobile/src/lib/session.js
+++ mobile/src/lib/session.js
@@ -5,7 +5,11 @@ let token = '';
 export async function login(username, password) {
     let opts = { username, password };
     let responseJson = await postJson('http://app.insideroil.com/login_rest', opts);
-    token = responseJson.token;
+    if(responseJson.token) {
+        token = responseJson.token;
+    } else {
+        throw responseJson.msg;
+    }
 }
 
 export function getToken() {
